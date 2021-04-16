b56fcc1f2abee6c5eb48842a3e77ad00481b0308
Amph <amprubio@ucm.es>
2021-04-16 16:57:07 +0200

    Practica 2 creada

---
+++ Practica 2/Practica0/ProyectosIG_x64_VS2019_FG/IG1App/Entity.h
@@ -0,0 +1,156 @@
+//#pragma once
+#ifndef _H_Entities_H_
+#define _H_Entities_H_
+
+#include <GL/freeglut.h>
+#include <glm.hpp>
+
+#include "Mesh.h"
+
+#include "Texture.h"
+
+//-------------------------------------------------------------------------
+
+class Abs_Entity  // abstract class
+{
+public:
+	Abs_Entity() : mModelMat(1.0) {};  // 4x4 identity matrix
+	virtual ~Abs_Entity() {};
+
+	Abs_Entity(const Abs_Entity& e) = delete;  // no copy constructor
+	Abs_Entity& operator=(const Abs_Entity& e) = delete;  // no copy assignment
+
+	virtual void render(glm::dmat4 const& modelViewMat) const = 0;  // abstract method
+	virtual void update() {}
+	// modeling matrix
+	glm::dmat4 const& modelMat() const { return mModelMat; };
+	void setModelMat(glm::dmat4 const& aMat) { mModelMat = aMat; };
+	void setColor(glm::dvec3 color) { mColor = glm::dvec4(color.r, color.g, color.b, 1); }
+	void setTexture(Texture* tex) { mTexture = tex; }; // establecer textura
+	void setTextureAlt(Texture* tex) { mTextureAlt = tex; }; // establecer textura
+
+protected:
+	Mesh* mMesh = nullptr;   // the mesh
+	Texture* mTexture = nullptr; // atributo para la textura principal
+	Texture* mTextureAlt = nullptr; // atributo para la textura alternativa
+	glm::dmat4 mModelMat;    // modeling matrix
+
+	glm::dvec4 mColor;    // modeling matrix
+	// transfers modelViewMat to the GPU
+	virtual void upload(glm::dmat4 const& mModelViewMat) const; 
+};
+//-------------------------------------------------------------------------
+
+class EjesRGB : public Abs_Entity 
+{
+public:
+	explicit EjesRGB(GLdouble l);
+	~EjesRGB() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+class Poligono : public Abs_Entity
+{
+
+public:
+	explicit Poligono(GLdouble numL, GLdouble rd);
+	~Poligono(){ delete mMesh; }
+	
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+
+};
+
+class TrianguloSierpinsky : public Abs_Entity
+{
+public:
+	explicit TrianguloSierpinsky(GLdouble rd, GLdouble numP);
+	~TrianguloSierpinsky() {delete mMesh; }
+
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+class TrianguloRGB : public Abs_Entity
+{
+public:
+	explicit TrianguloRGB(GLdouble rd);
+	~TrianguloRGB() {delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+	virtual void update();
+private:
+	double angleC, angleR; //angle es par
+};
+
+class RectanguloRGB : public Abs_Entity
+{
+public:
+	explicit RectanguloRGB(GLdouble w, GLdouble h);
+	~RectanguloRGB() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+class Estrella : public Abs_Entity
+{
+public:
+	explicit Estrella(GLdouble re, GLuint np, GLdouble h, double angle_y, double angle_z);
+	~Estrella() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+	virtual void update();
+
+private:
+	double angleY, angleZ;
+};
+
+class Caja : public Abs_Entity
+{
+public:
+	explicit Caja(GLdouble ld);
+	~Caja() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+class CajaFondo : public Abs_Entity
+{
+public:
+	explicit CajaFondo(GLdouble ld);
+	~CajaFondo() { delete mMesh; delete meshFondo; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+	virtual void update();
+private:
+	Mesh* meshFondo = nullptr;
+	glm::dmat4 mModelMatFondo = glm::dmat4(1.0);
+};
+
+class Suelo : public Abs_Entity
+{
+public:
+	explicit Suelo(GLdouble w, GLdouble h, GLuint rw, GLuint rh);
+	explicit Suelo(GLdouble w, GLdouble h);
+	~Suelo() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+class Foto : public Abs_Entity
+{
+public:
+	explicit Foto(GLdouble w, GLdouble h);
+	~Foto() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+	virtual void update();
+};
+
+
+class Cristalera :public Abs_Entity {
+public:
+	explicit Cristalera(GLdouble ld);
+	~Cristalera() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+class Planta:public Abs_Entity {
+public:
+	explicit Planta(GLdouble w, GLdouble h);
+	~Planta() { delete mMesh; };
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+#endif //_H_Entities_H_
---
+++ Practica 2/Practica0/ProyectosIG_x64_VS2019_FG/IG1App/Mesh.cpp
@@ -0,0 +1,345 @@
+#include "Mesh.h"
+#include "CheckML.h"
+#include <fstream>
+#include<glm.hpp>
+using namespace std;
+using namespace glm;
+
+//-------------------------------------------------------------------------
+
+void Mesh::draw() const 
+{
+  glDrawArrays(mPrimitive, 0, size());   // primitive graphic, first index and number of elements to be rendered
+}
+//-------------------------------------------------------------------------
+
+void Mesh::render() const 
+{
+  if (vVertices.size() > 0) {  // transfer data
+    // transfer the coordinates of the vertices
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_DOUBLE, 0, vVertices.data());  // number of coordinates per vertex, type of each coordinate, stride, pointer 
+    
+if (vColors.size() > 0) { // transfer colors
+      glEnableClientState(GL_COLOR_ARRAY);
+      glColorPointer(4, GL_DOUBLE, 0, vColors.data());  // components number (rgba=4), type of each component, stride, pointer  
+    }
+	if (vTextures.size() > 0) {
+		// activar array de coordenadas de textura
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glTexCoordPointer(2, GL_DOUBLE, 0, vTextures.data());
+	}
+
+	draw();
+
+	//Desactivamos TODO
+    glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+
+  }
+}
+//-------------------------------------------------------------------------
+
+Mesh * Mesh::createRGBAxes(GLdouble l)
+{
+  Mesh* mesh = new Mesh();
+
+  mesh->mPrimitive = GL_LINES;
+
+  mesh->mNumVertices = 6;
+  mesh->vVertices.reserve(mesh->mNumVertices);
+
+  // X axis vertices
+  mesh->vVertices.emplace_back(0.0, 0.0, 0.0);
+  mesh->vVertices.emplace_back(l, 0.0, 0.0);
+  // Y axis vertices
+  mesh->vVertices.emplace_back(0, 0.0, 0.0);
+  mesh->vVertices.emplace_back(0.0, l, 0.0);
+  // Z axis vertices
+  mesh->vVertices.emplace_back(0.0, 0.0, 0.0);
+  mesh->vVertices.emplace_back(0.0, 0.0, l);
+
+  mesh->vColors.reserve(mesh->mNumVertices);
+  // X axis color: red  (Alpha = 1 : fully opaque)
+  mesh->vColors.emplace_back(1.0, 0.0, 0.0, 1.0);
+  mesh->vColors.emplace_back(1.0, 0.0, 0.0, 1.0);
+  // Y axis color: green
+  mesh->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
+  mesh->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
+  // Z axis color: blue
+  mesh->vColors.emplace_back(0.0, 0.0, 1.0, 1.0);
+  mesh->vColors.emplace_back(0.0, 0.0, 1.0, 1.0);
+ 
+  return mesh;
+}
+
+Mesh* Mesh::generaPoligono(GLuint numL, GLdouble rd)
+{
+	Mesh* mesh = new Mesh();
+	mesh->mPrimitive = GL_LINE_LOOP;
+	mesh->mNumVertices = numL;
+	mesh->vVertices.reserve(mesh->mNumVertices);
+	
+	glm::dvec2 centro = { 0.0, 0.0 };
+	double ang= 3.1416 / 2; //angulo inicial 
+
+	//bucle que calcula n puntos 
+	for(int i=0; i < mesh->mNumVertices; i++)
+	{		
+		double x,y;
+		ang += (2 * 3.1416)/  numL ;
+		x = centro.x + rd * cos(ang);
+		y = centro.y + rd * sin(ang);
+		
+		mesh->vVertices.emplace_back(x, y, 0.0);
+	}
+	
+	return mesh;
+}
+
+Mesh* Mesh::generaSierpinski(GLdouble rd, GLuint numP)
+{
+	Mesh* mesh = new Mesh();
+	mesh->mPrimitive = GL_POINTS;
+	mesh->mNumVertices = numP;
+	mesh->vVertices.reserve(mesh->mNumVertices);
+
+	Mesh * triangulo = generaPoligono(3, rd); //triangulo de inicio
+
+	//pasamos los vertices del triangulo a nuestra mesh
+	for (int i = 0; i < triangulo->mNumVertices; i++)
+	{
+		mesh->vVertices.emplace_back(triangulo->vVertices[i]);
+	}
+	//calculamos el resto de puntos
+	for (int i = 2; i < mesh->mNumVertices; i++)
+	{
+		glm::dvec3 pA = mesh->vVertices[i]; //punto A desde donde empezamos a calcular
+		glm::dvec3 pB = mesh->vVertices[rand() % 3]; //punto B random
+	
+		glm::dvec3 pM; //punto medio
+		pM.x= (pA.x + pB.x )/2;
+		pM.y = (pA.y + pB.y) / 2;
+		pM.z = (pA.z + pB.z) / 2;
+
+		mesh->vVertices.emplace_back(pM );
+
+	}
+	
+	//borramos el triangulo porque ya no nos interesa
+	delete triangulo;
+	triangulo = nullptr;
+
+	return mesh;
+}
+
+Mesh* Mesh::generaTrianguloRGB(GLdouble rd)
+{
+	Mesh * mesh = generaPoligono(3, rd); //generamos un triangulo
+	mesh->mPrimitive = GL_TRIANGLES;
+	mesh->vColors.reserve(mesh->mNumVertices);
+		
+	// vector de colores
+	mesh->vColors.emplace_back(1.0,0.0,0.0,1.0);
+	mesh->vColors.emplace_back(0.0,1.0,0.0,1.0);
+	mesh->vColors.emplace_back(0.0,0.0,1.0,1.0);
+
+	return mesh; 
+}
+
+Mesh* Mesh::generaRectangulo(GLdouble w, GLdouble h)
+{
+	//w/2 y h/2 para que este centrado
+	GLdouble w_ = w / 2;
+	GLdouble h_ = h / 2;
+	
+	Mesh* mesh = new Mesh();
+	mesh->mPrimitive = GL_TRIANGLE_STRIP;
+	mesh->mNumVertices = 4;
+	mesh->vVertices.reserve(mesh->mNumVertices);
+
+	//como la primitiva es triangle strip tenemos que dar los vertices en diferente orden
+	mesh->vVertices.emplace_back(-w_, h_, 0.0);
+	mesh->vVertices.emplace_back(-w_, -h_, 0.0);
+	mesh->vVertices.emplace_back(w_, h_, 0.0);
+	mesh->vVertices.emplace_back(w_, -h_, 0.0);
+
+	return mesh;
+}
+
+Mesh* Mesh::generaRectanguloRGB(GLdouble w, GLdouble h)
+{
+	Mesh* mesh = generaRectangulo(w, h);
+	mesh->vColors.reserve(mesh->mNumVertices);
+
+	mesh->vColors.emplace_back(0.0, 0.0, 1.0, 1.0);
+	mesh->vColors.emplace_back(1.0, 0.0, 0.0, 1.0);
+	mesh->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
+	mesh->vColors.emplace_back(0.0, 0.0, 1.0, 1.0);
+
+	return mesh;
+}
+
+Mesh* Mesh::generaRectanguloConTextura(GLdouble w,GLdouble h,GLuint rw,GLuint rh ){
+	Mesh* mesh = generaRectangulo(w,h);
+	mesh->vTextures.reserve(mesh->mNumVertices);
+
+	mesh->vTextures.push_back(dvec2(0,rh));
+	mesh->vTextures.push_back(dvec2(0,0));
+	mesh->vTextures.push_back(dvec2(rw,rh));
+	mesh->vTextures.push_back(dvec2(rw,0));
+
+	return mesh;
+}
+
+Mesh* Mesh::generaContCubo(GLdouble ld)
+{
+	Mesh* mesh= new Mesh();
+	
+	mesh->mPrimitive = GL_TRIANGLE_STRIP;
+	mesh->mNumVertices = 10;
+
+	mesh->vVertices.reserve(mesh->mNumVertices);
+	//mesh->vColors.reserve(mesh->mNumVertices);
+	mesh->vTextures.reserve(mesh->mNumVertices);
+
+	GLdouble l=ld/2;
+
+	//1º cara (Z positiva)
+	mesh->vVertices.push_back(dvec3(-l,l,l)); //V0
+	mesh->vTextures.push_back(dvec2(0.0, 1.0));
+	mesh->vVertices.push_back(dvec3(-l,-l,l)); //V1
+	mesh->vTextures.push_back(dvec2(0.0, 0.0));
+	mesh->vVertices.push_back(dvec3(l,l,l)); //V2
+	mesh->vTextures.push_back(dvec2(1.0, 1.0));
+	mesh->vVertices.push_back(dvec3(l,-l,l)); //V3
+	mesh->vTextures.push_back(dvec2(1.0, 0.0));
+
+	//2º cara (Z negativa)
+	mesh->vVertices.push_back(dvec3(l,l,-l)); //V4
+	mesh->vTextures.push_back(dvec2(2.0, 1.0));
+	mesh->vVertices.push_back(dvec3(l,-l,-l)); //V5
+	mesh->vTextures.push_back(dvec2(2.0, 0.0));
+	mesh->vVertices.push_back(dvec3(-l,l,-l)); //V6
+	mesh->vTextures.push_back(dvec2(3.0, 1.0));
+	mesh->vVertices.push_back(dvec3(-l,-l,-l)); //V7
+	mesh->vTextures.push_back(dvec2(3.0, 0.0));
+
+	//luego enlazamos con los del principio con 2 vertices mas
+	mesh->vVertices.push_back(dvec3(-l,l,l)); //V0
+	mesh->vTextures.push_back(dvec2(4.0, 1.0));
+	mesh->vVertices.push_back(dvec3(-l,-l,l)); //V1
+	mesh->vTextures.push_back(dvec2(4.0, 0.0));
+
+	return mesh;
+}
+
+Mesh* Mesh::generaFondoCubo(GLdouble ld)
+{
+	Mesh* mesh = new Mesh;
+
+	mesh->mPrimitive = GL_TRIANGLE_STRIP;
+	mesh->mNumVertices = 4;
+
+	mesh->vVertices.reserve(mesh->mNumVertices);
+	//mesh->vColors.reserve(mesh->mNumVertices);
+	mesh->vTextures.reserve(mesh->mNumVertices);
+
+	GLdouble l = ld / 2;
+
+	mesh->vVertices.push_back(dvec3(-l, l, l)); //V0
+	mesh->vTextures.push_back(dvec2(0.0, 1.0));
+	mesh->vVertices.push_back(dvec3(-l, -l, l)); //V1
+	mesh->vTextures.push_back(dvec2(0.0, 0.0));
+	mesh->vVertices.push_back(dvec3(l, l, l)); //V2
+	mesh->vTextures.push_back(dvec2(1.0, 1.0));
+	mesh->vVertices.push_back(dvec3(l, -l, l)); //V3
+	mesh->vTextures.push_back(dvec2(1.0, 0.0));
+
+	return mesh;
+}
+
+Mesh* Mesh::generaEstrella3D(GLdouble re, GLuint np, GLdouble h)
+{
+	Mesh* mesh = new Mesh();
+	mesh->mPrimitive = GL_TRIANGLE_FAN;
+	mesh->mNumVertices = 2* (np +1);
+
+	mesh->vVertices.reserve(mesh->mNumVertices);
+	mesh->vVertices.push_back(dvec3(0,0,0));
+	
+
+	GLdouble ri = re / 2; //ri= radio interior
+	double ang= 3.1416 / 2; //angulo inicial
+
+ 
+	for(int i=0; i<mesh->mNumVertices-2; i++)
+	{
+		double x,y;
+		ang += (2 * 3.1416)/(mesh->mNumVertices-2);
+		if(i%2==0){ // circunferencia grande
+			x = re * cos(ang);
+			y = re * sin(ang);
+		}
+		else{ // circunferencia pequeña
+			x = ri * cos(ang);
+			y = ri * sin(ang);
+			
+
+		}
+		mesh->vVertices.push_back(dvec3(x, y, h));
+			
+	}
+	mesh->vVertices.push_back(mesh->vVertices[1]);
+	
+	return mesh;
+}
+
+Mesh* Mesh::generaEstrella3DconTextura(GLdouble re, GLuint np, GLdouble h)
+{
+	Mesh* mesh = new Mesh();
+	mesh->mPrimitive = GL_TRIANGLE_FAN;
+	mesh->mNumVertices = 2* (np +1);
+
+	mesh->vVertices.reserve(mesh->mNumVertices);
+	mesh->vColors.reserve(mesh->mNumVertices);
+	mesh->vTextures.reserve(mesh->mNumVertices);
+
+	mesh->vColors.push_back(dvec4(1.0, 1.0, 0.0, 1.0));
+	mesh->vVertices.push_back(dvec3(0,0,0));
+	mesh->vTextures.push_back(dvec2(0.5, 0.5));
+
+	GLdouble ri = re / 2; //ri= radio interior
+	double ang= 3.1416 / 2; //angulo inicial
+
+	double r_texture=0.5; // radio para la textura
+ 
+	for(int i=0; i<mesh->mNumVertices-2; i++){
+		double x,y;
+		ang += (2 * 3.1416)/(mesh->mNumVertices-2);
+		if(i%2==0){ // circunferencia grande
+			x = re * cos(ang);
+			y = re * sin(ang);
+			r_texture = 0.5;
+		}
+		else{ // circunferencia pequeña
+			x = ri * cos(ang);
+			y = ri * sin(ang);
+			r_texture=0.25;
+
+		}
+		double x_tex = 0.5 + r_texture * cos(ang);
+		double y_tex = 0.5 + r_texture * sin(ang);
+
+		mesh->vVertices.push_back(dvec3(x, y, h));
+		mesh->vColors.push_back(dvec4(1.0, 1.0, 0.0, 1.0));
+		mesh->vTextures.push_back(dvec2(x_tex, y_tex));
+	}
+	mesh->vVertices.push_back(mesh->vVertices[1]);
+	mesh->vColors.push_back(dvec4(1.0, 1.0, 0.0, 1.0));
+	mesh->vTextures.push_back(mesh->vTextures[1]);
+
+	return mesh;
+}
+
